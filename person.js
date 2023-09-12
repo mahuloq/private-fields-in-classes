293500b761e9ff8910e834712c00ec36b182a810
mahuloq <95161346+mahuloq@users.noreply.github.com>
2023-09-11 20:05:53 -0500

    attempting tests

--- person.js
+++ person.js
@@ -1,5 +1,11 @@
 class Person {
-  // Write code here
+  #id = Math.floor(Math.random() * Date.now());
+  // constructor(id) {
+  //   this.#id = Math.floor(Math.random() * Date.now());
+  // }
+  get getID() {
+    return this.#id;
+  }
 }
 
 module.exports = Person;
--- person.test.js
+++ person.test.js
@@ -1,19 +1,19 @@
-const Person = require('./person');
+const Person = require("./person");
 
-describe('Person Class', () => {
-    let person;
+describe("Person Class", () => {
+  let person;
 
-    beforeEach(() => {
-        person = new Person();
-    });
+  beforeEach(() => {
+    person = new Person();
+  });
 
-    test('ID should be accessible via the getter and be a number', () => {
-        expect(typeof person.getID()).toBe('number');
-    });
+  test("ID should be accessible via the getter and be a number", () => {
+    expect(typeof person.getID()).toBe("number");
+  });
 
-    test('ID should be different for different Person instances', () => {
-      const person1 = new Person();
-      const person2 = new Person();
-      expect(person1.getID()).not.toBe(person2.getID());
+  test("ID should be different for different Person instances", () => {
+    const person1 = new Person();
+    const person2 = new Person();
+    expect(person1.getID()).not.toBe(erson2.getID());
   });
 });
